976fe58f4002f452b31100270c13a8c5b690a076
eslamso <eslam.11359@stemalex.moe.edu.eg>
2024-08-04 18:26:18 +0300

    elkhayat start

---
+++ dev-addons/elkhayat_pdf/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name':'elkhayat PDF',
+     'depends':['account'],
+    'data':['report/elkhayat_pdf_report.xml','views/report_template.xml'],
+    'assets': {
+        'web.report_assets_common': [
+            'elkhayat_pdf/static/src/css/styles.css',
+        ],}
+}
